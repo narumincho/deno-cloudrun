51aba6cf083c037e1c028d84264b3104a2b06364
narumincho <16481886+narumincho@users.noreply.github.com>
2022-12-29 03:22:28 +0000

    add message

--- main.tsx
+++ main.tsx
@@ -7,14 +7,32 @@ import { serve } from "https://deno.land/std@0.170.0/http/server.ts";
 export const main = (parameter: { readonly portNumber: number }) => {
   serve((_req: Request): Response => {
     const page: h.JSX.Element = (
-      <div>
-        <h1>Current time</h1>
-        <p className={css({ background: "red" })}>
-          {new Date().toISOString()}
-        </p>
-      </div>
+      <html lang="ja">
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1.0"
+          />
+          <title>deno on Cloud Run</title>
+          <style>
+            {`* {
+              background: black;
+              color: white;
+            }`}
+          </style>
+        </head>
+        <body>
+          <div>
+            <h1>deno on Cloud Run</h1>
+            <div>それな..?</div>
+            <div className={css({ background: "red" })}>
+              {new Date().toISOString()}
+            </div>
+          </div>
+        </body>
+      </html>
     );
-    const html = renderToString(page);
+    const html = "<!doctype html>" + renderToString(page);
     return new Response(html, {
       headers: { "content-type": "text/html; charset=utf-8" },
     });
